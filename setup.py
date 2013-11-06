020a40d86ac66eb5c509f9b43f35d1f180e7ad80
Michał Bachowski <michal@bachowski.pl>
2013-11-06 15:22:16 +0100

    Removed 'install_requires' derective from setup.py in favour of *_requirements.txt; Added requirements files to MANIFEST

--- setup.py
+++ setup.py
@@ -18,6 +18,4 @@ setup(name='PyContentMakeUp',
     packages=['contentmakeup', 'contentmakeup.hook', 'contentmakeup.template',
         'contentmakeup.markup'],
     package_dir={'': 'src', 'hook': 'src/hook', 'template': 'src/template',
-        'markup': 'src/markup'},
-    install_requires=['PyPromise==1.1.0', 'pyspd==0.1.0', 'six==1.3.0',
-        'PyYAML>=3.10.0', 'pyspd=>0.2.0', 'pinject>=0.10.2'])
+        'markup': 'src/markup'})
