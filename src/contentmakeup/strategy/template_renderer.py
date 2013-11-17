b7028a48e60b8027f55e157f458ea4a0dee30f75
Michał Bachowski <michal@bachowski.pl>
2013-11-17 17:17:58 +0100

    Refactored template renderer to take into account that output_format is now dropped

--- src/contentmakeup/strategy/template_renderer.py
+++ src/contentmakeup/strategy/template_renderer.py
@@ -13,18 +13,16 @@ class TemplateRenderer(StrategyInterface):
         self.read_input = input_reader
         self._compiled = {}
 
-    def __call__(self, content, config, template_file, output_format):
-        return self._get_compiled(template_file, output_format)({'content': \
+    def __call__(self, content, config, template_file):
+        return self._get_compiled(template_file)({'content': \
                                                     content, 'config': config})
 
-    def _get_compiled(self, template_file, output_format):
+    def _get_compiled(self, template_file):
         if template_file not in self._compiled:
-            self._compiled[template_file] = self._compile(template_file, \
-                                                          output_format)
+            self._compiled[template_file] = self._compile(template_file)
         return self._compiled[template_file]
 
-    def _compile(self, template_file, output_format):
+    def _compile(self, template_file):
         template_type = self.discover_format(template_file)
-        return self.template_manager(template_type, \
-            output_format).compile(template_type, output_format, \
-            self.read_input(template_file))
+        return self.template_manager(template_type).compile(template_type, \
+                                                self.read_input(template_file))
